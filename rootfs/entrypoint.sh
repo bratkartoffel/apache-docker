0e9dbd716b57daa7811114131590e74ae4ff1b7d
Simon Frankenberger <simon@fraho.eu>
2023-06-06 05:48:11 +0200

    upgrade base image to alpine 3.18, add php 8.2

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -59,6 +59,9 @@ if [ "$(id -u)" -eq 0 ]; then
   elif [ -x /usr/sbin/php-fpm81 ]; then
     copyAndApplyVariables $APP_PHP_CONF_DIR /etc/php81
     APP_PHP_CONF_DIR=/etc/php81
+  elif [ -x /usr/sbin/php-fpm82 ]; then
+    copyAndApplyVariables $APP_PHP_CONF_DIR /etc/php82
+    APP_PHP_CONF_DIR=/etc/php82
   else
     echo ">>> no supported version of php found"
     rm -rv /etc/s6/php-fpm
